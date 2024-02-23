db8361f5f6b7eded7a9a88d29a9719d07db7cad2
Sleon4 <sergioleon4004@hotmail.com>
2024-02-22 21:09:29 -0500

    docs: classes, methods and properties have been documented

--- src/LionCommand/Kernel.php
+++ src/LionCommand/Kernel.php
@@ -6,10 +6,24 @@ namespace Lion\Command;
 
 use Symfony\Component\Console\Application;
 
+/**
+ * Adds functions to execute commands, allows you to create an Application
+ * object to run applications with your custom commands
+ *
+ * @package Lion\Command
+ */
 class Kernel
 {
+    /**
+     * [Initializes an application object to add commands]
+     *
+     * @var Application $application
+     */
     private Application $application;
 
+    /**
+     * [Class constructor]
+     */
     public function __construct()
     {
         $this->application = new Application();
@@ -17,7 +31,9 @@ class Kernel
 
     /**
      * Returns the current Application object
-     * */
+     *
+     * @return Application
+     */
     public function getApplication(): Application
     {
         return $this->application;
@@ -25,7 +41,11 @@ class Kernel
 
     /**
      * Change the current Application object
-     * */
+     *
+     * @param Application $application [Application object]
+     *
+     * @return Kernel
+     */
     public function setApplication(Application $application): Kernel
     {
         $this->application = $application;
@@ -35,7 +55,11 @@ class Kernel
 
     /**
      * Add assigned commands from an array
-     * */
+     *
+     * @param  array<Command> $commands [List of Command classes]
+     *
+     * @return void
+     */
     public function commands(array $commands): void
     {
         foreach ($commands as $command) {
@@ -45,7 +69,9 @@ class Kernel
 
     /**
      * Run the current application
-     * */
+     *
+     * @return void
+     */
     public function run(): void
     {
         $this->application->run();
@@ -53,7 +79,14 @@ class Kernel
 
     /**
      * Run commands either within a public in the project root
-     * */
+     *
+     * @param  string $command [Command to execute]
+     * @param  bool $index [indicates whether it is in 'public/index.php' or in
+     * the root of the project]
+     * @param  int $salt [Number of times to be returned in a '../' directory]
+     *
+     * @return array
+     */
     public function execute(string $command, bool $index = true, int $salt = 1): array
     {
         $data = [];
