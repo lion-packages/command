b721449ea896cf9da3360f22a8b4501a03c08f69
Sleon4 <sergioleon4004@hotmail.com>
2024-12-28 12:04:36 -0500

    chore: the namespace of the test classes is modified

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -2,25 +2,26 @@
 
 declare(strict_types=1);
 
-namespace Test;
+namespace Tests;
 
 use Lion\Command\Command;
 use Lion\Test\Test;
 
 class CommandTest extends Test
 {
-    const TEST = 'Test';
-    const ERROR_OUTPUT = "\033[0;31mTest\033[0m";
-    const SUCCESS_OUTPUT = "\033[0;32mTest\033[0m";
-    const WARNING_OUTPUT = "\033[0;33mTest\033[0m";
-    const INFO_OUTPUT = "\033[0;36mTest\033[0m";
-    const PURPLE_OUTPUT = "\033[0;95mTest\033[0m";
+    private const string TEST = 'Test';
+    private const string ERROR_OUTPUT = "\033[0;31mTest\033[0m";
+    private const string SUCCESS_OUTPUT = "\033[0;32mTest\033[0m";
+    private const string WARNING_OUTPUT = "\033[0;33mTest\033[0m";
+    private const string INFO_OUTPUT = "\033[0;36mTest\033[0m";
+    private const string PURPLE_OUTPUT = "\033[0;95mTest\033[0m";
 
     private $customCommand;
 
     public function setUp(): void
     {
-        $this->customCommand = new class extends Command {};
+        $this->customCommand = new class extends Command {
+        };
     }
 
     public function testErrorOutput(): void
